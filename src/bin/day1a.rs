42ec67f60453f854760f03ddfde3774546665d40
Jim Newsome <jnewsome@torproject.org>
2020-12-01 19:47:57 -0600

    Refactored find_sum_factors to find  factors

--- src/bin/day1a.rs
+++ src/bin/day1a.rs
@@ -2,8 +2,8 @@ use aoc2020::find_sum_factors;
 
 fn main() {
     use std::io::prelude::*;
-    let target : u32 = std::env::args().nth(1).expect("missing target").parse().expect("parsing target");
-    let xs : Vec<u32> = std::io::stdin().lock().lines().map(|s| s.unwrap().parse().expect("parsing line")).collect();
-    let (x, y) = find_sum_factors(xs.as_slice(), target).unwrap();
-    println!("{}", x*y);
+    let target : i32 = std::env::args().nth(1).expect("missing target").parse().expect("parsing target");
+    let xs : Vec<i32> = std::io::stdin().lock().lines().map(|s| s.unwrap().parse().expect("parsing line")).collect();
+    let factors = find_sum_factors(2, xs.as_slice(), target).unwrap();
+    println!("{}", factors.iter().fold(1, |x, y| x*y));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,54 @@
-/// Finds two numbers from `xs` that sum to `target`.
+/// Finds `n` elements of `xs` that sum to `target`. Returned in reverse order.
 ///
 /// ```
 /// use aoc2020::find_sum_factors;
-/// assert_eq!(find_sum_factors(&[1721, 979, 366, 299, 675, 1456],
-///                             2020),
-///            Some((1721, 299)));
-/// assert_eq!(find_sum_factors(&[],
-///                             2020),
-///            None);
-/// assert_eq!(find_sum_factors(&[1],
+///
+/// // n=1
+/// assert_eq!(find_sum_factors(1,
+///                             &[1721, 979, 366, 299, 675, 1456],
 ///                             2020),
 ///            None);
+/// assert_eq!(find_sum_factors(1,
+///                             &[1721, 979, 366, 299, 675, 1456],
+///                             366),
+///            Some(vec![366]));
+///
+/// // n=2
+/// assert_eq!(find_sum_factors(2,
+///                             &[1721, 979, 366, 299, 675, 1456],
+///                             2020),
+///            Some(vec![299, 1721]));
+/// assert_eq!(find_sum_factors(2,
+///                             &[],
+///                             2020),
+///            None);
+/// assert_eq!(find_sum_factors(2,
+///                             &[1],
+///                             2020),
+///            None);
+///
+/// // n=3
+/// assert_eq!(find_sum_factors(3,
+///                             &[1721, 979, 366, 299, 675, 1456],
+///                             2020),
+///            Some(vec![675, 366, 979]));
 /// ```
-pub fn find_sum_factors(xs: &[u32], target: u32) -> Option<(u32, u32)> {
-    for i in xs {
-        for j in xs.iter().skip(1) {
-            if i+j == target {
-                return Some((*i, *j))
+pub fn find_sum_factors(n: u32, xs: &[i32], target: i32) -> Option<Vec<i32>> {
+    match n {
+        1 => xs.iter().find(|x| **x == target).map(|x| vec![*x]),
+        _ => {
+            for i in 1..xs.len() {
+                let (head, tail) = xs.split_at(i);
+                let head : i32 = *head.last().unwrap();
+                match find_sum_factors(n-1, tail, target-head) {
+                    Some(mut v) => {
+                        v.push(head);
+                        return Some(v)
+                    },
+                    None => ()
+                }
             }
+            None
         }
     }
-    None
 }
