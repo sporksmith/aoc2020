5814709067b5e9dc96b3afec02adc87e542e121e
Jim Newsome <jnewsome@torproject.org>
2020-12-03 09:01:53 -0600

    Take angle as command-line arg

--- ci.sh
+++ ci.sh
@@ -19,4 +19,4 @@ test 1a `cargo run --bin day1 -- 2 2020 < inputs/1a.txt` 1019371
 test 1b `cargo run --bin day1 -- 3 2020 < inputs/1a.txt` 278064990
 test 2a `cargo run --bin password_check -- old < inputs/day2` 398
 test 2b `cargo run --bin password_check -- new < inputs/day2` 562
-test 3a `cargo run --bin toboggan < inputs/day3` 242
+test 3a `cargo run --bin toboggan -- 3 1 < inputs/day3` 242
