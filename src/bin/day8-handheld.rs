c81a7c55a1b617f0742c8489f02aab27028688ea
Jim Newsome <jnewsome@torproject.org>
2020-12-08 00:15:51 -0600

    Refactor for step 2

--- src/bin/day8-handheld.rs
+++ src/bin/day8-handheld.rs
@@ -1,10 +1,18 @@
 use std::collections::HashSet;
 use std::io::BufRead;
 
+#[derive(Debug)]
 enum Insn {
-    Nop,
+    Nop(i32),
     Jmp(i32),
     Acc(i32),
+}
+
+#[derive(Eq, PartialEq, Debug)]
+enum RunState {
+    Running,
+    Done,
+    Looped,
 }
 
 fn parse_line(line: &str) -> Insn {
@@ -12,7 +20,7 @@ fn parse_line(line: &str) -> Insn {
     let insn_str = insn_and_op.next().unwrap();
     let op: i32 = insn_and_op.next().unwrap().parse().unwrap();
     match insn_str {
-        "nop" => Insn::Nop,
+        "nop" => Insn::Nop(op),
         "jmp" => Insn::Jmp(op),
         "acc" => Insn::Acc(op),
         _ => panic!("Bad insn"),
@@ -26,33 +34,51 @@ fn parse_program<R: BufRead>(reader: R) -> Vec<Insn> {
         .collect()
 }
 
+#[derive(Debug)]
 struct Handheld {
     pc: usize,
     acc: i32,
+    state: RunState,
+    pcs_executed: HashSet<usize>,
+    program: Vec<Insn>,
 }
 
 impl Handheld {
-    fn new() -> Handheld {
-        Handheld { pc: 0, acc: 0 }
+    fn new(program: Vec<Insn>) -> Handheld {
+        Handheld {
+            pc: 0,
+            acc: 0,
+            state: RunState::Running,
+            pcs_executed: HashSet::new(),
+            program,
+        }
     }
 
-    fn execute(&mut self, insn: &Insn) {
-        match insn {
-            Insn::Nop => self.pc += 1,
+    fn step(&mut self) {
+        if self.state != RunState::Running {
+            panic!("Invalid run state");
+        }
+        match self.program[self.pc] {
+            Insn::Nop(_) => self.pc += 1,
             Insn::Jmp(i) => self.pc = (self.pc as i32 + i) as usize,
             Insn::Acc(i) => {
                 self.pc += 1;
                 self.acc += i
             }
+        };
+        if !self.pcs_executed.insert(self.pc) {
+            self.state = RunState::Looped;
+        }
+        if self.pc == self.program.len() {
+            self.state = RunState::Done;
         }
     }
 }
 
-fn acc_at_loop(program: &[Insn]) -> i32 {
-    let mut hh = Handheld::new();
-    let mut visited = HashSet::new();
-    while visited.insert(hh.pc) {
-        hh.execute(&program[hh.pc]);
+fn acc_at_loop(program: Vec<Insn>) -> i32 {
+    let mut hh = Handheld::new(program);
+    while hh.state != RunState::Looped {
+        hh.step();
     }
     hh.acc
 }
@@ -61,7 +87,7 @@ fn main() {
     let program = parse_program(std::io::stdin().lock());
     let part = std::env::args().nth(1).expect("missing part");
     let res = match part.as_str() {
-        "a" => acc_at_loop(&program),
+        "a" => acc_at_loop(program),
         _ => panic!("Bad part {}", part),
     };
     println!("{}", res);
@@ -85,6 +111,6 @@ acc +1
 jmp -4
 acc +6";
         let program = parse_program(Cursor::new(input.as_bytes()));
-        assert_eq!(acc_at_loop(&program), 5);
+        assert_eq!(acc_at_loop(program), 5);
     }
 }
