9b7bd51e76975477596ae983fab38fbe708e6c26
Jim Newsome <jnewsome@torproject.org>
2020-12-08 23:03:10 -0600

    Start migrating to a unified binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use aoc2020::*;
+
 fn main() {
-    println!("Hello, world!");
+    let part = std::env::args().nth(1).expect("missing part");
+    let res = match part.as_str() {
+        "7a" => bags::number_of_outer_bags_that_could_have_shiny(
+            &bags::parse_input(std::io::stdin().lock()).unwrap(),
+        ),
+        "7b" => bags::number_of_bags_in_shiny(
+            &bags::parse_input(std::io::stdin().lock()).unwrap(),
+        ),
+        "8a" => handheld::acc_at_loop(&handheld::parse_program(
+            std::io::stdin().lock(),
+        )) as usize,
+        "8b" => handheld::acc_after_fix(handheld::parse_program(
+            std::io::stdin().lock(),
+        )) as usize,
+        _ => panic!("Bad part {}", part),
+    };
+    println!("{}", res);
 }
