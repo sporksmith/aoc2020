13fa38312773cf0f72525a05cf325741768ddf43
Jim Newsome <jnewsome@torproject.org>
2020-12-11 00:36:31 -0600

    document

--- src/adapter.rs
+++ src/adapter.rs
@@ -25,6 +25,14 @@ pub fn part1(nums: &[u64]) -> u64 {
     ones * threes
 }
 
+pub fn part2(nums: &[u64]) -> u64 {
+    part2_helper(&mut HashMap::new(), nums[0], 1, nums)
+}
+
+// Recursive helper. Memoized via memo_table to make not insanely expensive.  Returns the number of
+// ways to connect the adapter `prev_val` to the adapters in `&nums[start_idx..]`. We pass
+// `start_idx` explicitly instead of the subslice itself, so that we can use `(prev_val,
+// start_idx)` as a cheap key into the memo table.
 fn part2_helper(
     memo_table: &mut HashMap<(u64, usize), u64>,
     prev_val: u64,
@@ -32,29 +40,38 @@ fn part2_helper(
     nums: &[u64],
 ) -> u64 {
     if let Some(res) = memo_table.get(&(prev_val, start_idx)) {
+        // We've already computed this. Return the previous result.
         return *res;
     }
+
+    // Alias for the slice of interest, for convenience.
     let slice = &nums[start_idx..nums.len()];
-    if slice.len() == 1 {
-        return 1;
-    }
+
+    // We should never be called with a zero-length slice (assuming sane original input).
+    debug_assert_ne!(slice.len(), 0);
+
+    // If the gap between the previous value and the first value in the slice is too large, then
+    // there are no ways to arrange the slice.
     if slice[0] - prev_val > 3 {
         return 0;
     }
+    // If there's only one value in the slice, then there's only one possibility.
+    if slice.len() == 1 {
+        return 1;
+    }
+    // Number of ways to arrange the slice if we include the first value of the slice.
     let ways_with_next =
         part2_helper(memo_table, slice[0], start_idx + 1, nums);
+    // Number of ways to arrange the slice if we *don't* include the first value of the slice.
     let ways_without_next = if slice[1] - prev_val > 3 {
         0
     } else {
         part2_helper(memo_table, prev_val, start_idx + 1, nums)
     };
     let res = ways_with_next + ways_without_next;
+    // Memoize recursive results, so that we don't need to recompute them later.
     memo_table.insert((prev_val, start_idx), res);
     res
-}
-
-pub fn part2(nums: &[u64]) -> u64 {
-    part2_helper(&mut HashMap::new(), nums[0], 1, nums)
 }
 
 #[cfg(test)]
