a754ddae37a00c8417e513f449d18aaba69f6c4d
Jim Newsome <jnewsome@torproject.org>
2020-12-12 11:50:08 -0600

    Refactor

--- src/d12_rain.rs
+++ src/d12_rain.rs
@@ -1,3 +1,12 @@
+use std::ops::{Add, Mul};
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum Direction {
+    Turn(Turn, u16),
+    Bearing(Bearing, u16),
+    Forward(u16),
+}
+
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum Turn {
     L,
@@ -12,11 +21,61 @@ pub enum Bearing {
     W,
 }
 
+impl Bearing {
+    fn to_vector(self) -> Vector {
+        match self {
+            Bearing::N => Vector { x: 0, y: 1 },
+            Bearing::E => Vector { x: 1, y: 0 },
+            Bearing::S => Vector { x: 0, y: -1 },
+            Bearing::W => Vector { x: -1, y: 0 },
+        }
+    }
+}
+
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
-pub enum Direction {
-    Turn(Turn, u16),
-    Bearing(Bearing, u16),
-    Forward(u16),
+pub struct Vector {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Vector {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Mul<i32> for Vector {
+    type Output = Self;
+
+    fn mul(self, rhs: i32) -> Self::Output {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
+
+impl Vector {
+    fn rotate(self, t: Turn, amt: i32) -> Vector {
+        let amt = (match t {
+            Turn::L => amt,
+            Turn::R => -amt,
+        } as f64)
+            .to_radians();
+        let sin = amt.sin();
+        let cos = amt.cos();
+        let x = self.x as f64;
+        let y = self.y as f64;
+        Vector {
+            x: (cos * x - sin * y) as i32,
+            y: (sin * x + cos * y) as i32,
+        }
+    }
 }
 
 pub fn parse(input: &str) -> Vec<Direction> {
@@ -41,69 +100,35 @@ pub fn parse(input: &str) -> Vec<Direction> {
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct BoatState {
-    x: i32,
-    y: i32,
-    b: Bearing,
+    pos: Vector,
+    bearing: Vector,
 }
 
 impl BoatState {
-    fn go_bearing(&mut self, b: Bearing, a: u32) {
-        let a = a as i32;
-        match b {
-            Bearing::N => self.y += a,
-            Bearing::S => self.y -= a,
-            Bearing::E => self.x += a,
-            Bearing::W => self.x -= a,
-        };
-    }
-
-    pub fn go(&mut self, d: &Direction) {
+    pub fn go_part1(&mut self, d: &Direction) {
         match d {
-            Direction::Turn(t, a) => self.b = self.b.turn(*t, *a),
-            Direction::Bearing(b, a) => self.go_bearing(*b, *a as u32),
-            Direction::Forward(a) => self.go_bearing(self.b, *a as u32),
+            Direction::Turn(t, a) => {
+                self.bearing = self.bearing.rotate(*t, *a as i32)
+            }
+            Direction::Bearing(b, a) => {
+                self.pos = self.pos + b.to_vector() * (*a as i32)
+            }
+            Direction::Forward(a) => {
+                self.pos = self.pos + self.bearing * (*a as i32)
+            }
         }
-    }
-
-    pub fn after(&self, d: &Direction) -> BoatState {
-        let mut res = self.clone();
-        res.go(d);
-        res
-    }
-}
-
-impl Bearing {
-    /// ```
-    /// use aoc2020::d12_rain::*;
-    /// assert_eq!(Bearing::N.turn(Turn::L, 90), Bearing::W);
-    /// assert_eq!(Bearing::E.turn(Turn::L, 180), Bearing::W);
-    ///
-    /// assert_eq!(Bearing::N.turn(Turn::R, 90), Bearing::E);
-    /// assert_eq!(Bearing::E.turn(Turn::R, 270), Bearing::N);
-    /// ```
-    pub fn turn(&self, d: Turn, a: u16) -> Bearing {
-        debug_assert_eq!(a % 90, 0);
-        let dirs = match d {
-            Turn::L => &[Bearing::N, Bearing::W, Bearing::S, Bearing::E],
-            Turn::R => &[Bearing::N, Bearing::E, Bearing::S, Bearing::W],
-        };
-        // There *must* be a more succinct way to express this!
-        let (offset, _) =
-            dirs.iter().enumerate().find(|(_, x)| x == &self).unwrap();
-        dirs[(offset + (a as usize) / 90) % dirs.len()]
     }
 }
 
 pub fn part1(directions: &[Direction]) -> u64 {
     let mut boat = BoatState {
-        x: 0,
-        y: 0,
-        b: Bearing::E,
+        pos: Vector { x: 0, y: 0 },
+        bearing: Bearing::E.to_vector(),
     };
     for d in directions {
-        boat.go(d);
+        boat.go_part1(d);
     }
-    (boat.x.abs() + boat.y.abs()) as u64
+    (boat.pos.x.abs() + boat.pos.y.abs()) as u64
 }
 
 #[cfg(test)]
@@ -121,53 +146,47 @@ R90
 F11",
         );
         let mut boat = BoatState {
-            x: 0,
-            y: 0,
-            b: Bearing::E,
+            pos: Vector { x: 0, y: 0 },
+            bearing: Bearing::E.to_vector(),
         };
-        boat.go(&input[0]);
+        boat.go_part1(&input[0]);
         assert_eq!(
             boat,
             BoatState {
-                x: 10,
-                y: 0,
-                b: Bearing::E
+                pos: Vector { x: 10, y: 0 },
+                bearing: Bearing::E.to_vector(),
             }
         );
-        boat.go(&input[1]);
+        boat.go_part1(&input[1]);
         assert_eq!(
             boat,
             BoatState {
-                x: 10,
-                y: 3,
-                b: Bearing::E
+                pos: Vector { x: 10, y: 3 },
+                bearing: Bearing::E.to_vector(),
             }
         );
-        boat.go(&input[2]);
+        boat.go_part1(&input[2]);
         assert_eq!(
             boat,
             BoatState {
-                x: 17,
-                y: 3,
-                b: Bearing::E
+                pos: Vector { x: 17, y: 3 },
+                bearing: Bearing::E.to_vector(),
             }
         );
-        boat.go(&input[3]);
+        boat.go_part1(&input[3]);
         assert_eq!(
             boat,
             BoatState {
-                x: 17,
-                y: 3,
-                b: Bearing::S
+                pos: Vector { x: 17, y: 3 },
+                bearing: Bearing::S.to_vector(),
             }
         );
-        boat.go(&input[4]);
+        boat.go_part1(&input[4]);
         assert_eq!(
             boat,
             BoatState {
-                x: 17,
-                y: -8,
-                b: Bearing::S
+                pos: Vector { x: 17, y: -8 },
+                bearing: Bearing::S.to_vector(),
             }
         );
 
