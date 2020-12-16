711b273850aff03de737af6fc326dae3a357c732
Jim Newsome <jnewsome@torproject.org>
2020-12-15 20:12:18 -0600

    Use a Vec instead of a HashMap
    80us -> 6us

--- src/d15_recitation.rs
+++ src/d15_recitation.rs
@@ -1,17 +1,17 @@
-use std::collections::HashMap;
-
 pub struct Game {
     prev_turn: u32,
     prev_num: u32,
-    last_spoken: HashMap<u32, u32>,
+    last_spoken: Vec<Option<u32>>,
 }
 
 impl Game {
-    pub fn new(input: &str) -> Game {
+    pub fn new(input: &str, max: usize) -> Game {
+        let mut v = Vec::new();
+        v.resize(max, None);
         let mut game = Game {
             prev_turn: 0,
             prev_num: 0,
-            last_spoken: HashMap::new(),
+            last_spoken: v,
         };
         for n in input.lines().next().unwrap().split(',') {
             let n = n.parse().unwrap();
@@ -22,7 +22,7 @@ impl Game {
 
     pub fn process_next(&mut self, n: u32) {
         if self.prev_turn > 0 {
-            self.last_spoken.insert(self.prev_num, self.prev_turn);
+            self.last_spoken[self.prev_num as usize] = Some(self.prev_turn);
         }
         self.prev_num = n;
         self.prev_turn += 1;
@@ -30,7 +30,8 @@ impl Game {
     }
 
     pub fn get_next(&self) -> u32 {
-        if let Some(prev_turn_spoken) = self.last_spoken.get(&self.prev_num) {
+        if let Some(prev_turn_spoken) = self.last_spoken[self.prev_num as usize]
+        {
             self.prev_turn - prev_turn_spoken
         } else {
             0
@@ -45,13 +46,13 @@ impl Game {
 }
 
 pub fn part1(input: &str) -> u32 {
-    let mut game = Game::new(input);
+    let mut game = Game::new(input, 2020);
     game.run_to(2020);
     game.prev_num
 }
 
 pub fn part2(input: &str) -> u32 {
-    let mut game = Game::new(input);
+    let mut game = Game::new(input, 30000000);
     game.run_to(30000000);
     game.prev_num
 }
